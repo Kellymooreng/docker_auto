383d89840df3638db2b29b4a90cba4a0cb706011
Kellymooreng <kellymooreng@yahoo.com>
2023-01-14 09:34:23 +0000

    Autobuild docker image
    Signed-off-by: Kellymooreng <kellymooreng@yahoo.com>

--- imagebuild.sh
+++ imagebuild.sh
@@ -1,1 +1,20 @@
-cd d    
+#!/bin/bash
+#script to automate docker image build
+#Author: Engr Kelechi
+#Maintainer: Kelechi
+#Company: Etech Consulting LLC
+today=`date`
+user=`whoami`
+dockeruser=kellymooreng
+password=Etechdevops4
+dockerrepo=autobuild_test1
+versions=( 18.04 20.04 21.10 16.04 22.04 )
+for i in ${versions[@]}; do
+docker build -t $dockeruser/$dockerrepo:$i --build-arg versions=groovy .
+docker login -u $dockeruser -p $password
+docker push $dockeruser/$dockerrepo:$i
+echo "$user is running a docker container with etechteam4/etecapp:$i on $today"
+sleep 5
+echo " Images have been uploaded to your dockerhub repository !! "
+done
+   
